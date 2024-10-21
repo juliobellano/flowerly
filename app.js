64321e5efa960eeb5005ec90a03159817b751ee9
Timss22 <timotiusrafaelthio@gmail.com>
2024-10-22 06:27:21 +0800

    new

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser'); 
-const app = express();
-const port = 3000;
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const PORT = 3000
 
+app.set("view engine", "pug") // pug as a template engine
+app.set("views", "./views")
 
-app.set('view engine', 'pug'); // pug as a template engine
-app.set('views', './views'); 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public")) // view static
 
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.static('public'));// view static
+app.get("/", (req, res) => {
+  res.render("index", { title: "JJB TEAM" })
+})
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'JJB TEAM'});
-});
-
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`);
-});
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`)
+})
