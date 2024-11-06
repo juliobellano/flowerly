ab5aee1363b57effa82b71616d4f12317f53df12
juliobellano <juliobellanolaksana@gmail.com>
2024-11-06 13:25:32 +0800

    add multiple page

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(express.static('public'));// view static
 app.get('/', (req, res) => {
   res.render('index', { title: 'JJB TEAM'});
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`);
