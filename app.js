3a11a5afc41c7ef62d95cc4e18af10a25057e2c6
Timss22 <timotiusrafaelthio@gmail.com>
2024-11-30 00:57:48 +0800

    added home page (blm kelar), ubah ui index.html dikit

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser'); 
-const app = express();
-const port = 3000;
-const path = require('path');
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const port = 3000
+const path = require("path")
 
+app.set("view engine", "pug") // pug as a template engine
+app.set("views", "./views")
 
-app.set('view engine', 'pug'); // pug as a template engine
-app.set('views', './views'); 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public")) // view static
 
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.static('public'));// view static
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+app.get("/", (req, res) => {
+     res.sendFile(path.join(__dirname, "views", "index.html"))
+})
+app.get("/about", (req, res) => {
+     res.render("about")
+})
+app.get("/home", (req, res) => {
+     res.sendFile(path.join(__dirname, "views", "home.html"))
+})
 
 app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`);
-});
+     console.log(`App is running on http://localhost:${port}`)
+})
